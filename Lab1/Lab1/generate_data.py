b25db2821ae1fbb1ccfec0f017499dbbe9d1f5e9
Sergei Irin <v-seirin@microsoft.com>
2020-10-01 21:20:38 +0300

    Result sends back by socket

--- Lab1/Lab1/generate_data.py
+++ Lab1/Lab1/generate_data.py
@@ -4,28 +4,45 @@ import numpy as np
 # Random matrix stream generator for
 # Hybrid computing labs
 # Usage:
-# <script_name>.py [stream_size_in_MegaBytes] [tcp_port]
+# <script_name>.py [stream_size_in_MegaBytes] [tcp_port_out] [tcp_port_in]
 # Produces stream with square matrix of floats
 
-def generate_stream_with_matrix(sizePort):
+def generate_stream_with_matrix(sizePorts):
     '''
     Function to generate file with
     random matrix of given size
     '''
-    sizeInBytes = float(sizePort[0]) * (2**20) # 1 Mb = 2^20 bytes
+    sizeInBytes = float(sizePorts[0]) * (2**20) # 1 Mb = 2^20 bytes
     num_rows_and_cols = int(math.sqrt(sizeInBytes / 11)) # 11 byte chars for each number
     matrix = np.random.rand(num_rows_and_cols, num_rows_and_cols)
-    print(matrix[0])
-    print(sum(matrix[0]) / len(matrix[0]))
+    print(' '.join([str(sum(vector) / len(vector)) for vector in matrix]))
     #np.savetxt(file_name + '.txt', matrix, fmt='%.8f', delimiter=' ')
     matrix_str = np.array2string(matrix, formatter={'float_kind':lambda x: "%.8f" % x}, separator=' ').replace('[','').replace(']','')
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', int(sizePort[1]))
+    server_address = ('localhost', int(sizePorts[1]))
     sock.connect(server_address)
 
     #print(matrix_str.encode('utf-8'))
     sock.sendall(matrix_str.encode('utf-8'))
+    sock.close()
+
+    # open connection to receive the result
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', int(sizePorts[2]))
+    sock.bind(server_address)
+    sock.listen(1)
+    connection, _ = sock.accept()
+
+    result = []
+    data = connection.recv(8)
+    while data:
+        result.append(data)
+        print(data)
+        data = connection.recv(8)
+    print(result)
+
+    connection.close()
     sock.close()
     
 
@@ -33,4 +50,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         generate_stream_with_matrix(sys.argv[1:])
     else:
-        print("Usage: <script_name>.py [stream_size_in_MegaBytes] [tcp_port]")
+        print("Usage: <script_name>.py [stream_size_in_MegaBytes] [tcp_port_out] [tcp_port_in]")
--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -81,6 +81,7 @@ void send_stream_to_port(int port, char* stream)
 
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
